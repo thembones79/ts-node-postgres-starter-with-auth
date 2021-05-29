c7a92b8528d1417ec2371f6061f049cf028b45cd
thembones79 <michal.szumnarski@gmail.com>
2021-05-29 13:31:58 +0200

    hf

--- src/app.ts
+++ src/app.ts
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV === "production",
+    secure: false,
   })
 );
 app.use(currentUser);
