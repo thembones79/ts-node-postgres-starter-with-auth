6f0d3082222831861b19275fa6a52d9364d87a55
Michał Szumnarski <michal.szumnarski@gmail.com>
2021-06-03 21:47:46 +0200

    Add files via upload

---
+++ src/config/dev.ts
@@ -0,0 +1,11 @@
+import { IKeys } from "./keys";
+
+export const keys: IKeys = {
+  JWT_SECRET: "jnrenernDDFttDRRsfjj3F8588b123",
+  CLIENT_ORIGIN: "http://localhost:3000",
+  PGHOST: "localhost",
+  PGPORT: "5432",
+  PGDATABASE: "starter",
+  PGUSER: "michal",
+  PGPASSWORD: "",
+};
